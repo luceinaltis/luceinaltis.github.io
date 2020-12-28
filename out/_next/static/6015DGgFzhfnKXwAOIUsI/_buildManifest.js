db2b37e26b63018eb1cad254bddbf3dbf6a09309
luceinaltis <luceinaltis2020@naver.com>
2020-12-29 07:28:23 +0900

    Deploy Next.js to gh-pages

--- out/_next/static/6015DGgFzhfnKXwAOIUsI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc39b4881d3c06665c5d4.css","static\u002Fchunks\u002Fpages\u002Findex-813570ead4c4a65cce5b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8c2857390bee7aa49ec9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
