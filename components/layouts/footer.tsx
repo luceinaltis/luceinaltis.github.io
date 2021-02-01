d7232a1d6434cdc3ea9613e973e16424d0851601
luceinaltis <luceinaltis2020@naver.com>
2021-02-02 01:05:21 +0900

    bug fix

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -5,7 +5,7 @@ import styles from '../../styles/layouts/Footer.module.scss'
 const Footer: NextComponentType = () => {
   return (
     <footer className={styles.footer}>
-      <div>Copyright {new Date().getFullYear()}. luce.log. All rights reserved.</div>
+      <div>copyright {new Date().getFullYear()}. luce.log. all rights reserved.</div>
     </footer>
   )
 }
